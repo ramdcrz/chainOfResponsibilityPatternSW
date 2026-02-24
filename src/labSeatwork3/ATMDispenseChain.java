7bb4583da5da135f0d3d626882c7f19cc2ccb205
Ramil Deocariza Jr. <ramildeocariza009@gmail.com>
2026-02-24 01:03:18 +0000

    Updated the flow of dispenses.

--- src/labSeatwork3/ATMDispenseChain.java
+++ src/labSeatwork3/ATMDispenseChain.java
@@ -6,8 +6,8 @@ public class ATMDispenseChain implements DispenseChain {
         // Initialize the chain
         this.nextChain = new Peso1000Dispenser();
         DispenseChain c2 = new Peso500Dispenser();
-        DispenseChain c3 = new Peso100Dispenser();
-        DispenseChain c4 = new Peso200Dispenser();
+        DispenseChain c3 = new Peso200Dispenser();
+        DispenseChain c4 = new Peso100Dispenser();
         DispenseChain c5 = new Peso50Dispenser();
         DispenseChain c6 = new Peso20Dispenser();
 
